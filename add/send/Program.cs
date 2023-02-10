d07743d2cd03fb5063cfe4a0e95dc3ea4b75bcfb
Tyler Gilmore <tgilmore@3cloudsolutions.com>
2023-02-10 10:05:31 -0600

    removed bin and obj files

--- add/send/Program.cs
+++ add/send/Program.cs
@@ -5,13 +5,22 @@ var factory = new ConnectionFactory { HostName = "localhost"};
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-
 channel.QueueDeclare(queue: "add", durable: false, exclusive: false, autoDelete: false, arguments: null);
-const string message = "1";
-var body = Encoding.UTF8.GetBytes(message);
 
-channel.BasicPublish(exchange: string.Empty, routingKey: "add", basicProperties: null, body: body);
-Console.WriteLine($" [x] Sent {message}");
+var continueInput = true;
+do
+{
+    Console.Write("Enter an integer (q to quit): ");
+    var input = Console.ReadLine();
 
-Console.WriteLine(" Press [enter] to exit.");
-Console.ReadLine();
+    if(input != null && input == "q" || input == "Q")
+    {
+        continueInput = false;
+    }
+    else
+    {
+        var body = Encoding.UTF8.GetBytes(input);
+        channel.BasicPublish(exchange: string.Empty, routingKey: "add", basicProperties: null, body: body);
+        Console.WriteLine($" [x] Sent {input}");
+    }
+} while (continueInput);
